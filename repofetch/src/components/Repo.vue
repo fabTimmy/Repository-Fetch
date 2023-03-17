6ae66a3a096d29463699c69cb222e8a023aa4e24
Fawale Oladele <26779885+fabTimmy@users.noreply.github.com>
2023-03-17 18:41:36 +0100

    repo fetch

--- repofetch/src/components/Repo.vue
+++ repofetch/src/components/Repo.vue
@@ -15,7 +15,7 @@
         <th>ID</th>
         <th>Name</th>
         <th>URL</th>
-        <th>Language</th>
+        <!-- <th>Language</th> -->
       </tr>
     </thead>
     <tbody>
@@ -26,7 +26,7 @@
         <router-link :to="`/singleRepo/${repo.id}`">
             {{ repo.html_url }}
           </router-link></td>
-        <td>{{ repo.language }}</td>
+        <!-- <td>{{ repo.language }}</td> -->
       </tr>
     </tbody>
     </table>
@@ -86,16 +86,57 @@ export default {
 * {
   font-family: "Outfit", sans-serif;
 }
-.links {
-  text-decoration: none;
-}
-.about router-link {
-  text-decoration: none;
-}
-.main {
+.main{
   margin: auto;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
 }
-table {
+table{
+  color: #666;
+  margin: 0 2rem;
+  border: 2px solid #ccc;
+  border-radius: 0.7rem;
+  background: rgb(245, 248, 248);
+  /* width: 60%; */
+  overflow: hidden !important;
+  min-width: 100%;
+}
+th {
+  background: #1c64ac;
+  color: #fff;
+  font-weight: 600;
+  font-size: 1rem;
+  text-align: center;
+}
+th,
+td {
+  border: 1px solid #ccc;
+  padding: 0.7rem 1.4rem !important;
+}
+caption {
+  text-align: center !important;
+  margin-top: 1rem;
+  margin-left: 7rem;
+  margin-bottom: 1rem;
+  font-weight: 600;
+  font-size: 1.7rem;
+  white-space: nowrap;
+  color: #fff;
+}
+@media (min-width: 1024px) {
+  
+  .links {
+    text-decoration: none;
+  }
+  .about router-link {
+    text-decoration: none;
+  }
+  .main {
+    margin: auto;
+  }
+  table {
+  table-layout: fixed !important;
   color: #666;
   margin: 0 2rem;
   border: 2px solid #ccc;
@@ -108,11 +149,17 @@ th {
   font-weight: 600;
   font-size: 1.2rem;
   text-align: center;
+  word-break: break-word !important;
 }
 th,
 td {
   border: 1px solid #ccc;
-  padding: 0.7rem 1.4rem;
+  padding: 0.7rem 1.4rem !important;
+}
+tr:hover td{
+  background: #cccccc67;
+  color: #666;
+  transition: 0.8s ease all;
 }
 caption {
   text-align: center !important;
@@ -136,5 +183,6 @@ nav {
   align-items: center;
   margin-top: 1rem;
   margin-bottom: 0.2rem;
+}
 }
 </style>
